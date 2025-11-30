68c4d987d1a185d38e41cb1e24832f24a2d0cde5
srobertsphd <samantha.roberts@gmail.com>
2025-11-29 22:47:23 -0500

    updated members.views to include reports_landing_view

--- members/views/__init__.py
+++ members/views/__init__.py
@@ -1,7 +1,11 @@
 from .search import landing_view, search_view
 from .members import member_detail_view, add_member_view, reactivate_member_view
 from .payments import add_payment_view
-from .reports import current_members_report_view
+from .reports import (
+    current_members_report_view,
+    reports_landing_view,
+    recent_payments_report_view,
+)
 from .health import healthz
 
 __all__ = [
@@ -12,5 +16,7 @@ __all__ = [
     "reactivate_member_view",
     "add_payment_view",
     "current_members_report_view",
+    "reports_landing_view",
+    "recent_payments_report_view",
     "healthz",
 ]
