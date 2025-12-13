e86c25ecb321390f8dd601a388f70cf5d6824c43
srobertsphd <samantha.roberts@gmail.com>
2025-12-13 13:19:29 -0500

    updated settings to load first

--- alano_club_site/settings.py
+++ alano_club_site/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "members",
+    "members.apps.MembersConfig",
 ]
 
 MIDDLEWARE = [
